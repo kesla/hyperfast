71d1c6d7143f6d05862d24c62a8e5f751f8f628d
David Björklund <david.bjorklund@gmail.com>
2015-02-24 12:06:49 +0100

    make tests pass

--- test/attr.js
+++ test/attr.js
@@ -5,12 +5,12 @@ var html = '<img class="a">';
 
 test('add attr', function (t) {
     t.plan(1);
-    
+
     var res = hyperglue(html, { 'img.a': { src: '/a.png' } }).innerHTML;
-    
+
     t.ok(
-        res === '<img class="a" src="/a.png" />'
-        || res === '<img src="/a.png" class="a" />',
+        res === '<img class="a" src="/a.png">'
+        || res === '<img src="/a.png" class="a">',
         'has both class and src: ' + res
     );
 });
@@ -19,33 +19,33 @@ test('preprend attr', function (t) {
     t.plan(1);
 
     var res = hyperglue(html, { 'img.a': { class: { prepend: 'b '} } }).innerHTML;
-    t.equal(res, '<img class="b a" />');
+    t.equal(res, '<img class="b a">');
 });
 
 test('append attr', function (t) {
     t.plan(1);
 
     var res = hyperglue(html, { 'img.a': { class: { append: ' b'} } }).innerHTML;
-    t.equal(res, '<img class="a b" />');
+    t.equal(res, '<img class="a b">');
 });
 
 test('append and prepend attr', function (t) {
     t.plan(1);
 
     var res = hyperglue(html, { 'img.a': { class: { prepend: 'pre ', append: ' post' } } }).innerHTML;
-    t.equal(res, '<img class="pre a post" />');
+    t.equal(res, '<img class="pre a post">');
 });
 
 test('append and prepend attr', function (t) {
     t.plan(1);
 
-    var res = hyperglue('<img />', { 'img': { class: { prepend: 'pre ', append: ' post' } } }).innerHTML;
-    t.equal(res, '<img class="pre  post" />');
+    var res = hyperglue('<img>', { 'img': { class: { prepend: 'pre ', append: ' post' } } }).innerHTML;
+    t.equal(res, '<img class="pre  post">');
 });
 
 test('append and prepend empty property', function (t) {
     t.plan(1);
-    
+
     var res = hyperglue('<div>so </div>', {
         'div': { 'class': { prepend: 'pre ', append: ' post' } }
     }).innerHTML;
@@ -54,7 +54,7 @@ test('append and prepend empty property', function (t) {
 
 test('append property with empty string', function (t) {
     t.plan(1);
-    
+
     var res = hyperglue('<div class="row">so </div>', {
         'div': { 'class': { append: '' } }
     }).innerHTML;
@@ -64,5 +64,5 @@ test('append property with empty string', function (t) {
 test('remove attr', function (t) {
     t.plan(1);
     var res = hyperglue(html, { 'img.a': { src: undefined } }).innerHTML;
-    t.equal(res, '<img class="a" />');
+    t.equal(res, '<img class="a">');
 });
